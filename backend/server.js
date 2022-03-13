b766b9632dca65f6b198e9a34d69dfe9219c4441
fawasam <fawasam5@gmail.com>
2022-03-13 10:47:08 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -32,22 +32,17 @@ app.use("/api/upload",uploadRoute)
 app.use('/uploads' , express.static(path.join(__dirname , '/uploads')))
 
 if(process.env.NODE_ENV === 'production'){
+
     app.use(express.static(path.join(__dirname, "/frontend/build")));
-    // app.use(morgan('dev'))
     app.get('*' , (req,res)=>{
         res.sendFile(path.resolve(__dirname , 'frontend' , 'build' , 'index.html'))
     })
-}else{
+}
+else{
     app.get('/' , (req,res)=>{
         res.send('API is Running')
     })
 }
-
-
-
-// app.get('*', (req, res) => {
-// //   res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
-// });
 
 
 
