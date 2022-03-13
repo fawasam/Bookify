afb7e21b18d25c3abc9ff8f13acc622ed4e561aa
fawasam <fawasam5@gmail.com>
2022-03-13 15:24:36 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -29,24 +29,24 @@ app.use(express.json())
 app.use("/api/users",userRoute)
 app.use("/api/books" ,bookRoute)
 app.use("/api/upload",uploadRoute)
-
-
-if(process.env.NODE_ENV === 'production'){
-
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
-    app.get('*' , (req,res)=>{
-        res.sendFile(path.resolve(__dirname , 'frontend' , 'build' , 'index.html'))
-    })
-}
-else{
-    app.get('/' , (req,res)=>{
-        res.send('API is Running')
-    })
-}
-
-
 //make uploads folder static
 app.use('/uploads' , express.static(path.join(__dirname , '/uploads')))
+
+
+// if(process.env.NODE_ENV === 'production'){
+
+//     app.use(express.static(path.join(__dirname, "/frontend/build")));
+//     app.get('*' , (req,res)=>{
+//         res.sendFile(path.resolve(__dirname , 'frontend' , 'build' , 'index.html'))
+//     })
+// }
+// else{
+//     app.get('/' , (req,res)=>{
+//         res.send('API is Running')
+//     })
+// }
+
+
 app.use(notFound)
 app.use(errorHandler)
 
